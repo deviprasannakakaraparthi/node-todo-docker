54b9b5a87327b9069a7826c698c7a4bf9ac70f92
prasanna <prasanna@DEVI>
2025-01-02 15:50:09 +0530

    commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+let todos = [
+  { id: 1, task: "Buy groceries" },
+  { id: 2, task: "Finish homework" }
+];
+
+app.get('/todos', (req, res) => {
+  res.json(todos);
+});
+
+app.post('/todos', (req, res) => {
+  const { task } = req.body;
+  const newTodo = { id: todos.length + 1, task };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+app.delete('/todos/:id', (req, res) => {
+  const { id } = req.params;
+  todos = todos.filter(todo => todo.id !== parseInt(id));
+  res.status(204).send();
+});
+
+app.listen(port, () => {
+  console.log(`Todo app listening at http://localhost:${port}`);
+});
+
